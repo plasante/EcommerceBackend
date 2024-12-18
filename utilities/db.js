b705b62a6c1ca0d99398c6db12fd96b2439dbd7d
Pierre Lasante <pierre.lasante@videotron.ca>
2024-12-18 16:11:05 -0500

    disable ssl when connecting to db

--- utilities/db.js
+++ utilities/db.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 module.exports.dbConnect = async() => {
     try {
         console.log(process.env.DB_URL);
-        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            ssl: false,
+        });
         console.log('Connected to database');
     } catch (error) {
         console.error('Error connecting to database:', error);
